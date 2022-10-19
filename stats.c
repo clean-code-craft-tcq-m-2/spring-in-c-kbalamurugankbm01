ad32820b5ce1314f0f7384779e5b60a0a99c1eab
kbalamurugankbm01 <115966525+kbalamurugankbm01@users.noreply.github.com>
2022-10-19 09:28:36 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,10 +1,17 @@
 #include "stats.h"
+#include "math.h"
+#include <stdio.h>
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
+    unsigned int index = 0;
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    
+    for (index = 0; index < setlength; index++) {
+        
+    }
 }
 
 int emailAlertCallCount = 0;
