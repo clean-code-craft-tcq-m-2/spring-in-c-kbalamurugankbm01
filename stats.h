8cfd5fc1a879bc6daa3de02e0a264e2d263940d9
kbalamurugankbm01 <115966525+kbalamurugankbm01@users.noreply.github.com>
2022-10-19 11:28:08 +0530

    Update stats.h
    Update

--- stats.h
+++ stats.h
@@ -1,8 +1,16 @@
+#include<stdio.h>
+#include "math.h"
 
-struct Stats compute_statistics(const float* numberset, int setlength);
+typedef struct {
+    float average;
+    float min;
+    float max;
+} Stats;
+
+Stats compute_statistics(const float* numberset, int setlength);
 
 typedef void (*alerter_funcptr)();
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], Stats computedStats);
 
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
